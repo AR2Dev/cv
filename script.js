3cde094648de76f2d2d3bf24319dbb5c814dbe75
Arles <arles.romero.rod@gmail.com>
2025-02-26 16:49:18 -0500

    nueva seccion

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 1, // Muestra una imagen por vez
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
